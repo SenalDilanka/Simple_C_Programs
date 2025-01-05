768614c3c7ec84a5a5463d1c05728e896b5c98c2
Senal Dilanka <senaldilanka461@gmail.com>
2025-01-05 15:46:49 +0530

    Create TrainTicket.c

---
+++ TrainTicket.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+	printf ("*****************************************************\n");
+	printf ("               Welcome to Tickets Online\n");
+	printf ("\n");
+	printf ("          Source:                     Anuradhapura\n");
+	printf ("          Destination:                Colombo Fort\n");
+	printf ("          Scheduled Time:             8.30 a.m.\n");
+	printf ("          Number of Passengers:       2\n");
+	printf ("          Sear Number(s)              20,21\n");
+	printf ("\n");
+	printf ("                  Have a Safe Journey! \n");
+	printf ("*****************************************************\n");
+	return 0;
+}
