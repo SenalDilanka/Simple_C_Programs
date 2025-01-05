062267624514818e3f958e15d526a64a1540a5e2
Senal Dilanka <senaldilanka461@gmail.com>
2025-01-05 17:53:08 +0530

    Create Read_a_string_and_display_it.c

---
+++ Read_a_string_and_display_it.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+ 
+int main() { 
+            char word[100]; 
+     
+            printf("Enter the word: "); 
+            scanf("%s",&word); 
+     
+            printf("The word is: %s\n",word); 
+            return 0; 
+}
